a451a206df35bd674a7af0f0d2152fa00ef42d01
Kapil Patel <kapilppatel03@gmail.com>
2022-06-25 11:49:43 +0530

    Added devise gem for user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  validates_confirmation_of :password
+  validates :dob, presence: true       
+end
