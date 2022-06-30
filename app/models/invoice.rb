988b890922dd2566a44be6c28463ff588942de51
Kapil Patel <kapilppatel03@gmail.com>
2022-06-30 23:20:40 +0530

    Remove redundant code

--- app/models/invoice.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Invoice < ApplicationRecord
-  belongs_to :user
-
-  validates :invoice_date, :amount, presence: true
-end
--- db/migrate/20220628091107_change_invoice_to_transaction.rb
+++
@@ -1,5 +0,0 @@
-class ChangeInvoiceToTransaction < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :invoices, :transactions
-  end
-end
